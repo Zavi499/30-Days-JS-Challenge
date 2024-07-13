f163449f85bf82f551ac297c50fdff5f803ac2b4
Zaviyaan <125255533+Zavi499@users.noreply.github.com>
2024-07-13 08:00:56 +0000

    Dayone file

---
+++ dayone.js
@@ -0,0 +1,47 @@
+// Activity 1 : Variable Declaration
+
+console.log("Activity 1");
+var number = 23;
+console.log(number);
+
+let string = "this is a string.";
+console.log(string);
+
+// Activity 2 : Const Declaration
+console.log("Activity 2");
+
+const bolen = true;
+console.log(bolen);
+
+// Activity 3 : Data Types
+console.log("Activity 3");
+
+let num = 20;
+let str = "This is Str";
+let bool = true;
+let obj = {
+    objName: "zavi",
+    objAge: "22",
+    objWeight: "55.5"
+}
+let arr = ["Zavi", "Hitesh", " Chaudary"];
+console.log(typeof (num));
+console.log(typeof (str));
+console.log(typeof (bool));
+console.log(typeof (obj));
+console.log(typeof (arr));
+
+// Activity 4 
+console.log("Activity : 4");
+
+let temp = 23;
+console.log(temp);
+temp = 25;
+console.log(temp);
+
+// Activity 5 
+console.log("Activity : 5");
+const net = 50;
+console.log(net);
+net = 55;
+console.log(net);
