00b2e309e30abed892730a5eed4fb6cb14f23a78
Zaviyaan <125255533+Zavi499@users.noreply.github.com>
2024-07-17 11:56:55 +0000

    Day 2

---
+++ day2.js
@@ -0,0 +1,46 @@
+// Activity 1 
+// Task 1
+console.log(2+2);
+// Task 2
+console.log(3-2);
+// Task 3
+console.log(2*2);
+// Task 4
+console.log(10/2);
+// Task 5
+console.log(10%5);
+
+// Activity 2
+// Task 6
+ 
+let num = 5;
+num += 5;
+console.log(num);
+
+// Task 7
+ 
+let secondNum = 5;
+secondNum -= 2;
+console.log(secondNum);
+
+
+// Activity 3
+// Task 8
+
+let first = 3<2;
+console.log(first);
+let second = 3>2;
+console.log(second);
+
+// Task 9
+let third = 3>=5;
+console.log(third);
+let fourth = 3<=5;
+console.log(fourth);
+
+// Task 10
+
+let numm = 3==5;
+console.log(numm);
+ numm = 3==="3";
+ console.log(numm);
